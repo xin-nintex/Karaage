1d0f01bf5e0096d26dcba4a89da8dda2ac420a27
xin-nintex <xin.ji@nintex.com>
2023-02-04 23:26:37 +1300

    feat(app): new action method

--- Karaage.Api/Controllers/WeatherForecastController.cs
+++ Karaage.Api/Controllers/WeatherForecastController.cs
@@ -29,4 +29,10 @@ public class WeatherForecastController : ControllerBase
             })
             .ToArray();
     }
+
+    [HttpGet]
+    public int GetDay()
+    {
+        return 3;
+    }
 }
