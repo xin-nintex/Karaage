c47180f9d54b22e4f780b33636961dd3b4b114c7
xin-nintex <xin.ji@nintex.com>
2023-02-06 01:24:27 +1300

    refactor(database): add missing migration

---
+++ Karaage.DataAccess/Migrations/20230205121900_jobDetails.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Karaage.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class jobDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "EntityElements",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(20,0)")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.CreateTable(
+                name: "JobDetails",
+                schema: "Processing",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EntityId = table.Column<long>(type: "bigint", nullable: false),
+                    JobId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobDetails_EntityElements_EntityId",
+                        column: x => x.EntityId,
+                        principalTable: "EntityElements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JobDetails_Jobs_JobId",
+                        column: x => x.JobId,
+                        principalSchema: "Processing",
+                        principalTable: "Jobs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobDetails_EntityId",
+                schema: "Processing",
+                table: "JobDetails",
+                column: "EntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobDetails_JobId",
+                schema: "Processing",
+                table: "JobDetails",
+                column: "JobId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobDetails",
+                schema: "Processing");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Id",
+                table: "EntityElements",
+                type: "numeric(20,0)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
